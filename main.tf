6d3eb657793d665b338ea1fa9b17fe4b7b2d38f6
Matthew-Bergin <matthew.bergin@paconsulting.com>
2021-04-14 10:29:56 +0100

    overwrite docker terraform with aws configuration test

--- main.tf
+++ main.tf
@@ -1,23 +1,22 @@
 terraform {
   required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
   }
 }
